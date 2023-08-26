7d7c43471d44f3fdfd96a9fc29c980b1ccef26c8
Enes BULUT <bulut0258@gmail.com>
2023-08-26 09:17:33 +0300

    Add project files.

---
+++ TemelMvcProje/Entitys/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TemelMvcProje.Entitys
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+}
---
+++ TemelMvcProje/Entitys/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TemelMvcProje.Entitys
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+        [Display(Name = "Soyad")]
+        public string LastName { get; set; }
+
+    }
+}
